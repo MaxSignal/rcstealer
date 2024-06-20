404b68fad870f8dff25af75ab6c35712cc7bf49a
XlinksToDo <89912994+MaxSignal@users.noreply.github.com>
2024-06-20 11:29:31 +0900

    add script for executable

--- sniffer_.py
+++ sniffer_.py
@@ -5,6 +5,7 @@ import time
 # cubedata_pattern = b'\x63\x75\x62\x65\x4D\x61\x70\x78\x00\x00'
 pattern = b'\x63\x6F\x6C\x6F\x75\x72\x4D\x61\x70\x78'
 ip_address = '162.19.204.42'  # 監視するIPアドレス
+# crf?:4533 singleplayer?:4537 multiplayer?:4541
 port = 4541  # 監視するポート番号
 
 data = ""
